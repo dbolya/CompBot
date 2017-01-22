d9b1dcd8657287ffc4bb44eb49ea8a58e7146781
dylan-mcleod <mcleoddylan0121@gmail.com>
2017-01-21 22:55:30 -0800

    Frontend troubles

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,38 @@
+
+
+
+var audio_context;
+var recorder;
+
+
+window.onload = function init() {
+    try {
+      // webkit shim
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+      window.URL = window.URL || window.webkitURL;
+      
+      audio_context = new AudioContext;
+      __log('Audio context set up.');
+      __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+    } catch (e) {
+      alert('No web audio support in this browser!');
+    }
+    
+    navigator.getUserMedia({audio: true}, startUserMedia, function(e) {
+      __log('No live audio input: ' + e);
+    });
+};
+
+var input = audio_context.createMediaStreamSource(stream);
+recorder = new Recorder(input);
+
 $('#display').mousedown(function(){
-  console.log('mouseDown');
+  recorder && recorder.record();
 });
 
 $('#display').mouseup(function(){
-  console.log("mouseUp");
+  recorder && recorder.stop();
 });
 
 
@@ -17,9 +46,5 @@ var t=setInterval(function(){
   ctx.fillRect(0,0,canvas.width,canvas.height);
 },16);
 
+recording = recorder.exportWAV()
 
-var microphone = Object.create(WaveSurfer.Microphone);
-
-microphone.init({
-    wavesurfer: wavesurfer
-});
