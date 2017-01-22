9bb2fa32619491dfa450bf1305184ec2efa59075
Danny Yang <dzy4@cornell.edu>
2017-01-21 20:08:31 -0800

    twitterbot update

--- training_strings/twitterbot.py
+++ training_strings/twitterbot.py
@@ -8,7 +8,13 @@ def getTweets(handle):
     try:
         app = Twython('EIULTBuVR8QSG8ZUo90hRemIb', 'wA7CggtcGsm89PBCbFatFiwvtO6ed26rIOc4oYmESRpbLMTYOK')
         user = app.lookup_user(screen_name=handle)
-        tweets = app.get_user_timeline(screen_name=handle, count=1000)
+        lastid=[808037199191166976]
+        tweets = []
+        for i in range(16):
+            a=app.get_user_timeline(screen_name=handle, count=200, max_id=lastid[-1])
+            tweets=tweets+a
+            lastid.append(a[-1]['id'])
+
         list_of_tweets=[html.unescape(tweet["text"].replace("\n", " ")) for tweet in tweets]
 
         file=open("twitterbot.txt","w")
@@ -25,4 +31,17 @@ def getTweets(handle):
     except TwythonError:
         raise RuntimeError("error!") from None
 
-getTweets('TheNiceBot')
+def parseFile():
+    file1=open("twitterbot.txt","r")
+    file2=open("newfile.txt","w")
+    list_of_tweets=[]
+    for line in file1:
+        if str(line) not in list_of_tweets:
+            list_of_tweets.append(str(line))
+    count=0
+    for tweet in list_of_tweets:
+        print(count)
+        count+=1
+        file2.write(tweet)
+
+parseFile()
