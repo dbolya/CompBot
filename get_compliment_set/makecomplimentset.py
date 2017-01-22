9d4ad82fae1fc3e88c4512e63d1f6adbf594e128
dylan-mcleod <mcleoddylan0121@gmail.com>
2017-01-21 21:25:20 -0800

    Added TIL

--- get_compliment_set/makecomplimentset.py
+++ get_compliment_set/makecomplimentset.py
@@ -9,17 +9,16 @@ client_id='ru1yiiBSUuOqaw',\
 client_secret="dVHE9aZQ1wfLKE98VI2ZEJ-dEKU", password='HackTheWorldThisIsAmericaBaby',\
 user_agent='BaronOBullets', username='BaronOBullets')
 
-compFile = open("toastme_top.txt", 'w+')
+compFile = open("todayilearned_top.txt", 'w+')
+
+badstuff = ["http", ".com", "imgur", "r/", "reddit", "+", "*", "-", "photo", "pic", "selfie", "hell", "shit", "damn", "fuck", "ass", "cunt", "edit", "sexy", "porn", "boob", "tit", "(", ")", "[", "]", "rack", "pussy", "\"", "\'", "autis", "butt", "weed", "bake", "420", "bastard", "roast", "link", "report", "sub", "fag", "fgt", "retard", "mongoloid", "nig", "m9", "two", "couple", "each", "wrote", "title", "post", "submission", "karma", "upvot", "downvot", "op", "thank", "sex", "bitch", "compliment", "thumbnail", "background", "btw", "everyone", "u/", "toast", "username"]
+badstarters = ["also", "and", "but"] # Hide the fact that this is taken from a reddit thread :^)
 
 
 def filterPost(submission):
 	if( (submission.link_flair_text is None or submission.link_flair_text.lower() == "selfie" or submission.score > 2) and (not "official" in submission.title.lower()) ):
 		return True
 	return False
-	
-	
-badstuff = ["http", ".com", "imgur", "r/", "reddit", "+", "*", "-", "photo", "pic", "selfie", "hell", "shit", "damn", "fuck", "ass", "cunt", "edit", "sexy", "porn", "boob", "tit", "(", ")", "[", "]", "rack", "pussy", "\"", "\'", "autis", "butt", "weed", "bake", "420", "bastard", "roast", "link", "report", "sub", "fag", "fgt", "retard", "mongoloid", "nig", "m9", "two", "couple", "each", "wrote", "title", "post", "submission", "karma", "upvot", "downvot", "op", "thank", "sex", "bitch", "compliment", "thumbnail", "background", "btw", "everyone", "u/", "toast", "username"]
-badstarters = ["also", "and", "but"] # Hide the fact that this is taken from a reddit thread :^)
 
 def filterComment(comment,submission):
 	lowercase = comment.body.lower();
@@ -34,6 +33,18 @@ def filterComment(comment,submission):
 			return False
 	if(submission.author.name.lower() in lowercase):
 		return False
+	
+	return True
+	
+badstuffTIL = ["shit", "damn", "fuck", "ass", "cunt", "sexy", "porn", "boob", "pussy", "weed", "bake", "420", "bastard", "sub", "fag", "retard", "each", "sex", "bitch", "u/", "toast"]
+	
+def filterTIL(submission):
+	lowercase = submission.title.lower();
+	if( (not ("til" in lowercase or "today i learned" in lowercase) ) ):
+		return False
+	for st in badstuffTIL:
+		if(st in lowercase):
+			return False
 	
 	return True
 	
@@ -52,7 +63,26 @@ def processAndSubmitComment(comment):
 		
 	compFile.write(nString + "\n")
 	print(nString)
-			
+	
+def processAndSubmitTIL(post):
+	nString = post.strip()
+	hasEndingP = False
+	for p in endingpunctuation:
+		if(nString.endswith(p)):
+			hasEndingP = True
+	if(hasEndingP == False):
+		nString += "."
+	nString = nString.replace("\n", " ")
+	
+	nString = nString[:1].upper() + nString[1:]
+	
+	nString = nString.replace("TIL", "Did you know")
+		
+	compFile.write(nString + "\n")
+	print(nString)
+	
+
+'''	
 sub = instance.subreddit('ToastMe')
 dat = sub.parse
 
@@ -74,3 +104,22 @@ for post in sub.top(limit=500):
 				print("error parsing comment, skipping")
 	except:
 		print("error parsing post, skipping")
+'''
+
+sub = instance.subreddit('TodayILearned')
+
+modulo = 0
+count=0
+for post in sub.top(limit=1000):
+	modulo += 1
+	if(modulo % 10 == 0):
+		compFile.flush()
+	try:
+		if filterTIL(post) == False:
+			continue
+		else:
+			count+=1
+			processAndSubmitTIL(post.title);
+	except:
+		print("error parsing post, skipping")
+print(count)
