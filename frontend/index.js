02291aa6206fed87627ac44d8ff4b69450d1e21d
dylan-mcleod <mcleoddylan0121@gmail.com>
2017-01-21 22:11:16 -0800

    Frontend stuff

--- frontend/index.js
+++ frontend/index.js
@@ -5,9 +5,21 @@ $('#display').mousedown(function(){
 $('#display').mouseup(function(){
   console.log("mouseUp");
 });
+
+
+var wavesurfer = Object.create(WaveSurfer);
+
+
 var t=setInterval(function(){
   var canvas=document.getElementById("display");
   var ctx=canvas.getContext('2d');
   ctx.fillStyle='#222222';
   ctx.fillRect(0,0,canvas.width,canvas.height);
 },16);
+
+
+var microphone = Object.create(WaveSurfer.Microphone);
+
+microphone.init({
+    wavesurfer: wavesurfer
+});
